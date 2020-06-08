d905f5f241c2f5931441449ed98624690c170465
Anthony Ting <anthonydting@gmail.com>
2020-06-07 23:09:24 -0700

    make default /playtime list playtime of all players

--- src/main/java/ca/anthonyting/personalplugins/commands/PlayTime.java
+++ src/main/java/ca/anthonyting/personalplugins/commands/PlayTime.java
@@ -9,7 +9,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Locale;
+import java.util.*;
 
 public class PlayTime implements CommandExecutor {
 
@@ -21,6 +21,20 @@ public class PlayTime implements CommandExecutor {
         String timePlayedAsString = String.format(Locale.ENGLISH, "%1$,.2f", timePlayedInHours);
         return ChatColor.DARK_AQUA + playerName + ChatColor.WHITE + " has played on this server for " +
                 ChatColor.DARK_AQUA + timePlayedAsString + ChatColor.WHITE + " hours.";
+    }
+
+    private String customPlayerHasPlayedMessage(String prefix, OfflinePlayer target, String suffix) {
+        return prefix + normalizeTimePlayed(getTimePlayedInHours(target)) + suffix;
+    }
+
+    private Double getTimePlayedInHours(OfflinePlayer offlinePlayer) {
+        int timePlayedInSeconds = offlinePlayer.getStatistic(Statistic.PLAY_ONE_MINUTE)/20;
+        int secondsInAnHour = 3600;
+        return (double) timePlayedInSeconds / secondsInAnHour;
+    }
+
+    private String normalizeTimePlayed(Double timePlayed) {
+        return String.format(Locale.ENGLISH, "%1$,.2f", timePlayed);
     }
 
     private Double getTimePlayedInHours(String targetName) {
@@ -38,8 +52,17 @@ public class PlayTime implements CommandExecutor {
         }
         int timePlayedInSeconds = target.getStatistic(Statistic.PLAY_ONE_MINUTE)/20;
         int secondsInAnHour = 3600;
-        String timePlayedInHours = String.format(Locale.ENGLISH, "%1$,.2f", ((double) timePlayedInSeconds) / secondsInAnHour);
         return (double) timePlayedInSeconds / secondsInAnHour;
+    }
+
+    private String getFormattedMessage(OfflinePlayer player, int position, boolean bold, boolean newline) {
+        String boldString = "";
+        if (bold) {
+            boldString += ChatColor.DARK_AQUA.toString();
+        }
+
+        String prefix = boldString + position + ". " + player.getName() + ": ";
+        return customPlayerHasPlayedMessage(prefix, player, " hours" + (newline ? "\n" : "") + ChatColor.RESET);
     }
 
     @Override
@@ -53,8 +76,60 @@ public class PlayTime implements CommandExecutor {
                 commandSender.sendMessage(playerHasPlayedMessage(strings[0], timePlayed));
             }
         } else if (commandSender instanceof Player) {
+            // TODO consider storing result
             Player p = (Player) commandSender;
-            commandSender.sendMessage(playerHasPlayedMessage(p.getName(), getTimePlayedInHours(p.getName())));
+            SortedMap<Double, OfflinePlayer> allPlayers = new TreeMap<>(Collections.reverseOrder());
+            double totalTimePlayed = 0;
+            double currentTimePlayed;
+            for (OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers()) {
+                currentTimePlayed = getTimePlayedInHours(offlinePlayer);
+                totalTimePlayed += currentTimePlayed;
+                allPlayers.put(currentTimePlayed, offlinePlayer);
+            }
+
+            StringBuilder playersShown = new StringBuilder();
+            String serverTotal = ChatColor.GOLD + "Server total: " + ChatColor.RED + normalizeTimePlayed(totalTimePlayed) + " hours\n" + ChatColor.RESET;
+
+            final int maxLineCount = 10;
+            boolean playerFound = false;
+            Iterator<Map.Entry<Double, OfflinePlayer>> iterator = allPlayers.entrySet().iterator();
+            Map.Entry<Double, OfflinePlayer> currentEntry;
+            boolean bold;
+            int lineCount = 1; // from serverTotal
+            int playerRank = 1;
+            while (iterator.hasNext() && (lineCount <= maxLineCount - 2)) { // reserve two lines for showing server total and self if not in top
+                currentEntry = iterator.next();
+                if (!playerFound && currentEntry.getValue().equals(p)) {
+                    bold = true;
+                    playerFound = true;
+                } else {
+                    bold = false;
+                }
+                playersShown.append(getFormattedMessage(currentEntry.getValue(), playerRank++, bold, iterator.hasNext()));
+                lineCount++;
+            }
+
+            if (!playerFound) {
+                // always show this player
+                while (iterator.hasNext() && lineCount <= maxLineCount - 1) {
+                    currentEntry = iterator.next();
+                    if (currentEntry.getValue().equals(p)) {
+                        // add new line if there is a next element and the line is not the last one
+                        playersShown.append(getFormattedMessage(currentEntry.getValue(), playerRank, true, iterator.hasNext() && lineCount != maxLineCount - 1));
+                        lineCount++;
+                        playerFound = true;
+                    }
+                    playerRank++;
+                }
+            } else {
+                // show remaining lines if this player showed already
+                while (iterator.hasNext() && lineCount <= maxLineCount - 1) {
+                    currentEntry = iterator.next();
+                    playersShown.append(getFormattedMessage(currentEntry.getValue(), playerRank++, false, iterator.hasNext() && lineCount != maxLineCount - 1));
+                    lineCount++;
+                }
+            }
+            commandSender.sendMessage(serverTotal + playersShown.toString());
         }
 
         return true;
